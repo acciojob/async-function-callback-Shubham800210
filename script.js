1b365ff8f379cef830584dd97947094233019abd
shubham kumar <65395298+Shubham800210@users.noreply.github.com>
2023-03-21 16:05:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+// const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+async function fetchData(callback) {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    callback(data);
+  }
+  
+  function displayMessage(data) {
+      document.getElementById("output").innerText = data.title;
+  }
